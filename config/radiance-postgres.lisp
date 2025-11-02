30b2d88f6ae901b685fe98f01cec64f0048d6492
Brian O'Reilly <fade@deepsky.com>
2025-11-02 15:40:15 -0500

    small moves, ellie. small moves.

--- config/radiance-postgres.lisp
+++ config/radiance-postgres.lisp
@@ -7,7 +7,6 @@
 (setf (config :database :connection)
       '(:type :postgres
         :host "localhost"  ; Change to "asteroid-postgres" when running in Docker
-        ;; :host "asteroid-postgres"
         :port 5432
         :database "asteroid"
         :username "asteroid"
