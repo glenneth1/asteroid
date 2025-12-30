6721d57550ee2d5c488eaf8136d1e724549ed1c2
Brian O'Reilly <fade@deepsky.com>
2025-12-30 15:24:13 -0500

    Unneeded things...
    no need for the postgres/docker env, and we don't track
    playlists/stream-queue.m3u because it is dynamically generated.

--- docker/operations/db_backup.sh
+++ docker/operations/db_backup.sh
@@ -7,13 +7,12 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Source the runtime environment for asteroid.radio. creds, paths &c
 
-if [[ -f ${HOME}/SourceCode/lisp/asteroid/docker/environment.sh ]]; then
-    . ${HOME}/SourceCode/lisp/asteroid/docker/environment.sh
-fi
+# if [[ -f "${HOME}"/SourceCode/lisp/asteroid/docker/environment.sh ]]; then
+#     source "${HOME}"/SourceCode/lisp/asteroid/docker/environment.sh
+# fi
 
 BACKUP_DIR=${SCRIPT_DIR}/db_backups
 mkdir -p "$BACKUP_DIR"
 
-docker exec asteroid-postgres sh -c 'pg_dump $POSTGRES_DB -U $POSTGRES_USER -F p' | xz --compress -c > ${BACKUP_DIR}/db_$(date +%Y-%m-%d-%H.%M.%S)_backup.sql.xz
-
+docker exec asteroid-postgres sh -c 'pg_dump $POSTGRES_DB -U $POSTGRES_USER -F p' | xz --compress -c > "${BACKUP_DIR}"/db_"$(date +%Y-%m-%d-%H.%M.%S)"_backup.sql.xz
 
