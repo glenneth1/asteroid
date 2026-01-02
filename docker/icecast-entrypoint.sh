821352edb7209f97b0bf9a99a1972dc0e9ccea08
glenneth <glennt40@gmail.com>
2026-01-02 11:52:53 +0300

    feat: Add ICECAST_HOSTNAME config for YP directory registration
    - Add hostname substitution in icecast-entrypoint.sh
    - Update environment.sh.template with ICECAST_HOSTNAME option
    - Defaults to localhost for dev, production sets ice.asteroid.radio
    - Fixes YP directories showing localhost:8000 as stream URL

--- docker/icecast-entrypoint.sh
+++ docker/icecast-entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 # Generate icecast.xml from base config
-# If ICECAST_ENABLE_YP=true, insert YP directory blocks
+# - Substitute hostname (defaults to localhost for dev)
+# - If ICECAST_ENABLE_YP=true, insert YP directory blocks
 
 cp /etc/icecast.xml.base /etc/icecast.xml
+
+# Set hostname (defaults to localhost if not specified)
+ICECAST_HOSTNAME=${ICECAST_HOSTNAME:-localhost}
+echo "Icecast hostname: $ICECAST_HOSTNAME"
+sed -i "s|<hostname>localhost</hostname>|<hostname>$ICECAST_HOSTNAME</hostname>|" /etc/icecast.xml
 
 if [ "$ICECAST_ENABLE_YP" = "true" ]; then
     echo "YP directory publishing ENABLED"
